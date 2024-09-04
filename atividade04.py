240ca2cf1fd080e5231e1e8fb043cf6dfe81edc5
vitorlopes1248 <vitorlopes1248@gmail.com>
2024-09-04 15:28:44 -0300

    atividade04 nota do aluno

---
+++ atividade04.py
@@ -0,0 +1,7 @@
+nota1 = int(input("digite sua primeira nota: "))
+nota2 = int(input("digite sua segunda nota: "))
+nota3 = int(input("digite sua terceira nota: "))
+
+media = (nota1 + nota2 + nota3) /3
+
+print("sua nota: ", media)
